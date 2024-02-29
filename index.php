51b90b5f93ca35721a7c55d37d671e623ce7f663
HyunjiiKim <hjkim96727@gmail.com>
2024-02-29 15:56:53 +0100

    final

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,19 +6,70 @@
     <title>Document</title>
 </head>
 <body>
-    <header style="display: flex;">
-        <div id="logo"></div>
-        <div id="banner"></div>
+    <header style="width: 1440px; height: 120px; margin: 0 auto; display: flex; align-items: center; justify-content: space-between;">
+        <div id="logo" style="height: 100px; width: 20%; margin-left: 15px;"><a href="instagram.com"><img src="https://www.meilleure-innovation.com/wp-content/uploads/2022/04/logo-instagram.png" style="height: 100%; weight: 100%;"></a></div>
+        <div id="banner" style="width: 75%; height: 100px; "><img src="https://fastly.picsum.photos/id/240/1080/100.jpg?hmac=WdEaZ10fNDGHcBA5KXzMuxMHYoCR_1MPbB6OfOxIEUQ"></div>
     </header>
-    <section>
-        <div id="contain1"></div>
-        <div id="contain2" style="display:flex;">
-            <div class="subcontain"></div>
-            <div class="subcontain"></div>
-            <div class="subcontain"></div>
+    <nav style="width: 1440px; margin: 0px auto; height: 20px;"><ul style=" display: flex; justify-content: space-between; font-size: 16pt; list-style-type:none;">
+        <li>Home</li>
+        <li >About</li>
+        <li>article</li>
+        <li>contact</li>
+    </ul></nav>
+    <section style="width:1440px; margin: 20px auto;">
+        <div id="contain1" style="height: 350px; background-color: #67B6BE;">
+            <h1>Bienvenue chez moi ! </h1>
+            <p>Ce site est développé par moi si vous avez questions, n'hesitez pas à m'écrire.</p>
+        </div>
+        <div id="contain2" style="display:flex; height: 350px; flex-grow:1;">
+            <div class="subcontain" style=" width: 33.3%; height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+                <div class="content" style="width: 80%; height: 80%; overflow: clip; display: flex; flex-direction: column; align-items: center;">
+                    <h3>The Imperative Role of Responsive Web Design in Modern Development</h3>
+                    <h5>Crafting User-Centric Experiences Across Devices</h5>
+                    <p>In the digital age, where users access websites through a myriad of devices, from smartphones and tablets to desktop computers, responsive web design has emerged as a foundational principle in modern web development. Responsive design ensures that websites dynamically adjust and adapt their layout, content, and functionality to suit the screen size and capabilities of the device being used.
+
+        In this article, we delve into the significance of responsive web design and its impact on user experience, accessibility, and search engine optimization (SEO). We explore the key principles behind responsive design, including fluid grids, flexible images, and media queries, which enable websites to seamlessly transition across different devices and screen resolutions.
+
+        Furthermore, we discuss best practices for implementing responsive design, such as prioritizing content hierarchy, optimizing loading times, and testing across various devices and browsers. By embracing responsive web design, developers can create user-centric experiences that engage and delight users across the digital landscape.</p>
+                </div>
+                <div class="pubInfo" style="width: 80%; display: flex; font-size: 14px; justify-content: space-between;">
+                    <p>13:42</p>
+                    <p>29.02.2024</p>
+                </div>
+            </div>
+            <div class="subcontain" style=" width: 33.3%; height: 100%; display: flex; flex-direction: column; align-items: center;">
+                <div class="content" style="width: 80%; height: 80%; overflow: clip; display: flex; flex-direction: column; align-items: center;">
+                    <h3>The Imperative Role of Responsive Web Design in Modern Development</h3>
+                    <h5>Crafting User-Centric Experiences Across Devices</h5>
+                    <p>In the digital age, where users access websites through a myriad of devices, from smartphones and tablets to desktop computers, responsive web design has emerged as a foundational principle in modern web development. Responsive design ensures that websites dynamically adjust and adapt their layout, content, and functionality to suit the screen size and capabilities of the device being used.
+
+        In this article, we delve into the significance of responsive web design and its impact on user experience, accessibility, and search engine optimization (SEO). We explore the key principles behind responsive design, including fluid grids, flexible images, and media queries, which enable websites to seamlessly transition across different devices and screen resolutions.
+
+        Furthermore, we discuss best practices for implementing responsive design, such as prioritizing content hierarchy, optimizing loading times, and testing across various devices and browsers. By embracing responsive web design, developers can create user-centric experiences that engage and delight users across the digital landscape.</p>
+                </div>
+                <div class="pubInfo" style="width: 80%; display: flex; font-size: 14px; justify-content: space-between;">
+                    <p>13:42</p>
+                    <p>29.02.2024</p>
+                </div>
+            </div>
+            <div class="subcontain" style=" width: 33.3%; height: 100%; display: flex; flex-direction: column;">
+                <div class="content" style="width: 80%; height: 80%; overflow: clip; display: flex; flex-direction: column; align-items: center;">
+                    <h3>The Imperative Role of Responsive Web Design in Modern Development</h3>
+                    <h5>Crafting User-Centric Experiences Across Devices</h5>
+                    <p>In the digital age, where users access websites through a myriad of devices, from smartphones and tablets to desktop computers, responsive web design has emerged as a foundational principle in modern web development. Responsive design ensures that websites dynamically adjust and adapt their layout, content, and functionality to suit the screen size and capabilities of the device being used.
+
+        In this article, we delve into the significance of responsive web design and its impact on user experience, accessibility, and search engine optimization (SEO). We explore the key principles behind responsive design, including fluid grids, flexible images, and media queries, which enable websites to seamlessly transition across different devices and screen resolutions.
+
+        Furthermore, we discuss best practices for implementing responsive design, such as prioritizing content hierarchy, optimizing loading times, and testing across various devices and browsers. By embracing responsive web design, developers can create user-centric experiences that engage and delight users across the digital landscape.</p>
+                </div>
+                <div class="pubInfo" style="width: 80%; display: flex; font-size: 14px; justify-content: space-between;">
+                    <p>13:42</p>
+                    <p>29.02.2024</p>
+                </div>
+            </div>
         </div>
     </section>
-    <footer></footer>
+    <footer style="width: 1440px; height: 200px; margin: auto; border: 1px solid red; display: flex; align-items: center; justify-content: center;"> <div>Copyright Hyunji KIM. All rights reserved.</div>
+</footer>
 </body>
 </html>
-?>
