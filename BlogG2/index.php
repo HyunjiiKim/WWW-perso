3e3d076032da5e69114a05e88816fb0f8d16188f
HyunjiiKim <hjkim96727@gmail.com>
2024-02-29 14:23:13 +0100

    1

---
+++ BlogG2/index.php
@@ -0,0 +1,25 @@
+<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header style="display: flex;">
+        <div id="logo"></div>
+        <div id="banner"></div>
+    </header>
+    <section>
+        <div id="contain1"></div>
+        <div id="contain2" style="display:flex;">
+            <div class="subcontain"></div>
+            <div class="subcontain"></div>
+            <div class="subcontain"></div>
+        </div>
+    </section>
+    <footer></footer>
+</body>
+</html>
+?>
